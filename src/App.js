d0a6c409c15cd7c41e0e2694932deb1d3a8c208c
Zak Mayfield <zmayfield@parkbenchsolutions.com>
2022-07-04 17:55:57 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
 import './App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Map, { Marker, Popup } from 'react-map-gl';
+import axios from 'axios';
+import ship from './assets/ship.PNG';
+import { useEffect, useState } from 'react';
+import opencage from 'opencage-api-client';
+import { data } from './data/data';
 
 function App() {
+  const [viewState, setViewState] = useState({
+    longitude: '',
+    latitude: '',
+    zoom: 2,
+  });
+  const [markerPosition, setMarkerPosition] = useState({
+    longitude: '',
+    latitude: '',
+  });
+
+  // REPLACE THIS STATE WITH {} WHEN DONE WITH DUMMY DATA
+  const [currentLocationData, setCurrentLocationData] = useState(data);
+  const [showPopup, setShowPopup] = useState(true);
+
+  console.log(data);
+
+  useEffect(() => {
+    axios
+      .get('http://api.open-notify.org/iss-now.json')
+      .then((res) => {
+        setViewState({
+          ...viewState,
+          longitude: res.data.iss_position.longitude,
+          latitude: res.data.iss_position.latitude,
+        });
+
+        setMarkerPosition({
+          longitude: res.data.iss_position.longitude,
+          latitude: res.data.iss_position.latitude,
+        });
+
+        // callOpenCage(
+        //   res.data.iss_position.latitude,
+        //   res.data.iss_position.longitude
+        // );
+      })
+      .catch();
+  }, []);
+
+  const callOpenCage = (lat, lon) => {
+    opencage
+      .geocode({
+        q: `${lat}, ${lon}`,
+        key: '4cf0f5f55bd7487e935f15ca4c209938',
+      })
+      .then((data) => {
+        console.log('result for testing no component error', data);
+        setCurrentLocationData(data.results[0].components);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  };
+
+  // useEffect(() => {
+  //   console.log('----->', showPopup);
+  // }, [showPopup]);
+
+  if (!viewState.latitude || !viewState.longitude) return;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Map
+      {...viewState}
+      onMove={(e) => {
+        setViewState(e.viewState);
+      }}
+      style={{ width: '80vw', height: '80vh' }}
+      mapStyle='mapbox://styles/zaar/cl4q1117q000w15qyg4pyef6s'
+      mapboxAccessToken='pk.eyJ1IjoiemFhciIsImEiOiJjbDRwd2s4NDEwYmZhM2pscHFoNmd2cXgyIn0.rBjxlh-4PyvOnILgV2gJXg'
+    >
+      <Marker
+        latitude={markerPosition.latitude}
+        longitude={markerPosition.longitude}
+        style={{ cursor: 'pointer' }}
+        onClick={(e) => {
+          e.originalEvent.stopPropagation();
+          setShowPopup(!showPopup);
+        }}
+      >
+        <img
+          style={{ width: '40px', height: '40px' }}
+          alt='ship map marker'
+          src={ship}
+        />
+      </Marker>
+
+      {showPopup && (
+        <Popup
+          longitude={markerPosition.longitude}
+          latitude={markerPosition.latitude}
+          anchor='top'
+          offset={17}
+          onClose={() => setShowPopup(false)}
+          closeOnClick
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {currentLocationData.country
+            ? currentLocationData.country
+            : currentLocationData.body_of_water}
+        </Popup>
+      )}
+    </Map>
   );
 }
 
 export default App;
+
+// function App() {
+//   const [viewport, setViewport] = useState({
+//     longitude: '',
+//     latitude: '',
+//     zoom: 2,
+//   });
+
+//   useEffect(() => {
+//     axios
+//       .get('http://api.open-notify.org/iss-now.json')
+//       .then((res) =>
+//         setViewport({
+//           ...viewport,
+//           longitude: res.data.iss_position.longitude,
+//           latitude: res.data.iss_position.latitude,
+//         })
+//       )
+//       .catch();
+//   }, []);
+
+//   useEffect(() => {
+//     console.log(viewport);
+//   }, [viewport]);
+
+//   if (!viewport.latitude || !viewport.longitude) return;
+
+//   return (
+//     <div>
+//       <Map
+//         initialViewState={{ ...viewport }}
+//         style={{ width: '75vw', height: '75vh' }}
+//         mapStyle='mapbox://styles/zaar/cl4q1117q000w15qyg4pyef6s'
+//         mapboxAccessToken='pk.eyJ1IjoiemFhciIsImEiOiJjbDRwd2s4NDEwYmZhM2pscHFoNmd2cXgyIn0.rBjxlh-4PyvOnILgV2gJXg'
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/data/data.js
@@ -0,0 +1,62 @@
+export const data = {
+  documentation: 'https://opencagedata.com/api',
+  licenses: [
+    {
+      name: 'see attribution guide',
+      url: 'https://opencagedata.com/credits',
+    },
+  ],
+  rate: {
+    limit: 2500,
+    remaining: 2495,
+    reset: 1656374400,
+  },
+  results: [
+    {
+      bounds: {
+        northeast: {
+          lat: 40.306761,
+          lng: 85.0029397,
+        },
+        southwest: {
+          lat: 38.0479734,
+          lng: 83.3831404,
+        },
+      },
+      components: {
+        'ISO_3166-1_alpha-2': 'CN',
+        'ISO_3166-1_alpha-3': 'CHN',
+        'ISO_3166-2': ['CN-XJ'],
+        _category: 'place',
+        _type: 'city',
+        continent: 'Asia',
+        country: 'China',
+        country_code: 'cn',
+        county: 'Qarqan County',
+        region: 'Bayingolin Mongol Autonomous Prefecture',
+        state: 'Xinjiang',
+        town: 'Tazhong',
+      },
+      confidence: 1,
+      formatted: 'Tazhong, Qarqan County, China',
+      geometry: {
+        lat: 39.0187565,
+        lng: 83.6121555,
+      },
+    },
+  ],
+  status: {
+    code: 200,
+    message: 'OK',
+  },
+  stay_informed: {
+    blog: 'https://blog.opencagedata.com',
+    twitter: 'https://twitter.com/OpenCage',
+  },
+  thanks: 'For using an OpenCage API',
+  timestamp: {
+    created_http: 'Mon, 27 Jun 2022 16:59:12 GMT',
+    created_unix: 1656349152,
+  },
+  total_results: 1,
+};
